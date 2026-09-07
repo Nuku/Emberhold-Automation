d42cd1dc0e57af5bccdddb8142c7e86d6c8f78fa
David Silver <dms.silver@gmail.com>
2026-09-07 12:45:45 -0700

    Bump version for power priority update

--- emberhold_automation.user.js
+++ emberhold_automation.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Emberhold Automation
 // @namespace    https://github.com/emberhold
-// @version      1.28.0
+// @version      1.28.1
 // @description  Configurable automation for Emberhold
 // @updateURL    https://raw.githubusercontent.com/Nuku/Emberhold-Automation/main/emberhold_automation.user.js
 // @downloadURL  https://raw.githubusercontent.com/Nuku/Emberhold-Automation/main/emberhold_automation.user.js
