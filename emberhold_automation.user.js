251d87bab0209d01d90715387c81edce562e5543
David Silver <dms.silver@gmail.com>
2026-09-15 15:19:55 -0700

    Prefer current power job identity

--- emberhold_automation.user.js
+++ emberhold_automation.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Emberhold Automation
 // @namespace    https://github.com/emberhold
-// @version      1.33.0
+// @version      1.33.1
 // @description  Configurable automation for Emberhold
 // @updateURL    https://raw.githubusercontent.com/Nuku/Emberhold-Automation/main/emberhold_automation.user.js
 // @downloadURL  https://raw.githubusercontent.com/Nuku/Emberhold-Automation/main/emberhold_automation.user.js
@@ -957,8 +957,10 @@
       // The game's power telemetry omits resource for the Sky Metal Forge,
       // even though it fills Aluminum storage like a direct production site.
       if (site.id === 'aluminumWorks') return 'aluminum';
-      if (site.resource) return site.resource;
       const jobId = typeof site.job === 'string' ? site.job : site.job?.id;
+      if (typeof site.job === 'string' && Object.hasOwn(state.res || {}, site.job)) {
+        return site.job;
+      }
       if (site.job && typeof site.job === 'object') {
         if (site.job.res || site.job.resource || site.job.output) {
           return site.job.res || site.job.resource || site.job.output;
@@ -970,6 +972,7 @@
       if (jobDef?.res || jobDef?.resource || jobDef?.output) {
         return jobDef.res || jobDef.resource || jobDef.output;
       }
+      if (site.resource) return site.resource;
       // Powered buildings can support workers rather than produce directly.
       // Use that worker's output when ranking queued resource shortages.
       return Object.values(jobs).find(job => job.poweredBuilding === site.id)?.res;
